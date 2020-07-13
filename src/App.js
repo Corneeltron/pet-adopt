2f0f190308953e6050e5ceccfffbfb7edad9d942
Cornelius Phanthanh <corneliusphanthanh@gmail.com>
2020-07-13 12:47:34 +0900

    Added custom hooks to clean up redundant components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Flappy",
-      animal: "Bird",
-      breed: "Parakeet",
-    }),
-    React.createElement(Pet, {
-      name: "Waffle",
-      animal: "Dog",
-      breed: "Mutt",
-    }),
-    React.createElement(Pet, {
-      name: "Snowball",
-      animal: "Bird",
-      breed: "Parakeet",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
